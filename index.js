199f84a51d71c593a441299f42d849f40d4bad42
roysaucedo <roy.a.saucedo@gmail.com>
2022-04-26 11:53:45 -0700

    Button Challenge

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard
-let hasBlackjack = false
-let isALive = true
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackjack = false;
+let isALive = true;
+let message = "";
 
-console.log(sum)
+console.log(sum);
 
-if (sum <= 20) {
-    console.log("Do you want to draw another card? 😁")
-} else if (sum === 21) {
-    console.log("Woohoo! You've got Blackjack! 🎉")
-    hasBlackjack = true
-} else {
-    console.log("You're out of the game. 🤮")
-    isALive = false
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw another card? 😁";
+  } else if (sum === 21) {
+    message = "Woohoo! You've got Blackjack! 🎉";
+    hasBlackjack = true;
+  } else {
+    message = "You're out of the game. 🤮";
+    isALive = false;
+  }
+
+  // CASH OUT
+  console.log(hasBlackjack);
+
+  // ALIVE
+  console.log(isALive);
+
+  console.log(message);
 }
-
-// CASH OUT
-console.log(hasBlackjack)
-
-// ALIVE
-console.log(isALive)
