a94c981ee4bb29f50e4795ce95f60ab345198fc0
roysaucedo <roy.a.saucedo@gmail.com>
2022-06-01 11:39:35 -0700

    new changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 10;
 let secondCard = 4;
+let cards = [firstCard, secondCard] //array - ordered list
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isALive = true;
@@ -19,7 +20,7 @@ function startGame() {
 
 function renderGame() {
   sumEl.textContent = "Sum: " + sum
-  cardsEl.textContent = "Cards: " + firstCard + " + " + secondCard
+  cardsEl.textContent = "Cards: " + cards[0] + " + " + cards[1]
   if (sum <= 20) {
     message = "Do you want to draw another card?";
   } else if (sum === 21) {
@@ -34,8 +35,9 @@ function renderGame() {
 }
 
 function newCard() {
-  console.log("Drawing a new card from the deck!")
-  let card = 7
+  let card = 6
   sum += card
+  cards.push(card)
+  console.log(cards)
   renderGame()
 }
